a5938ddda4870e7aa14b402b0e6242cfc98a3ca6
ishu_19 <ishuraniugdia@gmail.com>
2020-06-22 19:11:34 +0530

    added basic class base views for group

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from . import models
+from groups.models import Group
+from django.views.generic import CreateView,DetailView,ListView,RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
 # Create your views here.
+
+class Creategroup(CreateView):
+    model=Group
+    fields=('name','description')
+    success_url=reverse_lazy('groups:all')
+
+class Singlegroup(DetailView):
+    model=Group
+
+class Listgroup(ListView):
+    model=Group
