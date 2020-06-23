6554a21771406b960d397101e571451679c1a018
ishu_19 <ishuraniugdia@gmail.com>
2020-06-23 21:31:57 +0530

    created urls for group app

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Group,Groupmember
 # Register your models here.
+
+admin.site.register(Group)
+admin.site.register(Groupmember)
+
+class GroupmemberInline(admin.TabularInline):
+    model = Groupmember
---
+++ groups/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,re_path
+from groups.views import Creategroup,Singlegroup,Listgroup
+
+app_name='groups'
+
+urlpatterns = [
+    path('new/',Creategroup.as_view(),name="create"),
+    path('',Listgroup.as_view(),name="all"),
+    path('posts/in/(?P<slug>[-\w]+)/',Singlegroup.as_view(),name="single"),
+
+]
